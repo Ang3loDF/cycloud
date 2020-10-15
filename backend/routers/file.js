f3874a1a81204a7b8a4a5e980f0199b448bf058c
Angelo <angelodifuccia02@gmail.com>
2020-10-15 21:37:18 +0200

    add route for getting and removing a file

--- backend/routers/file.js
+++ backend/routers/file.js
@@ -1,3 +1,5 @@
+const { route } = require("./user");
+
 let express = require("express"),
 	router = express.Router(),
 	multer = require("multer"),
@@ -29,7 +31,7 @@ const storage = multer.diskStorage({
 
 		// get file original name and extension
 		let original = file.originalname.split(".");
-		const extention = original.pop();
+		const extention = original.pop().toLowerCase();
 		let name = original.join();
 
 		// if there is already a file with this name in the user's folder,
@@ -45,7 +47,7 @@ const storage = multer.diskStorage({
 let upload = multer({ storage });
 
 /* 
- * Route for uploading files. The user can send a request to this route with a file
+ * Route for uploading files. The user can send a request to this route with a
    file in the body, that will be stored in his folder.
  * Body params (form-data):
 	- file: the file that have to be uploaded
@@ -66,6 +68,76 @@ router.post("/upload", auth, upload.single("file"), (req, res) => {
 		user.files.push(req.file.filename);
 		user.save((err, user) => {
 			res.json({ success: true, message: "File uploaded" });
+		});
+	});
+});
+
+/* 
+ * Route for getting a single file. An authenticated user can get a file from
+   his folder on the server through this route.
+ * URL params:
+	- filename: tha name of the file to get (case sensitive)
+*/
+router.get("/files/:filename", auth, (req, res) => {
+	const { id } = req.user;
+	const { filename } = req.params;
+
+	// find the user
+	User.findOne({ _id: id }, (err, user) => {
+		if (err || !user) {
+			return res.json({
+				success: false,
+				message: "Error in getting the file",
+			});
+		}
+		// check if the name of the file is in the user's list
+		if (!user.files.includes(filename)) {
+			return res.json({
+				success: false,
+				message: "File doesn't exist",
+			});
+		}
+		// send the file
+		res.sendFile(filename, {
+			root: uploadsPath + id,
+		});
+	});
+});
+
+/* 
+ * Route for removing a file. An authenticated user can remove a file from
+   his folder on the server through this route.
+ * URL params:
+	- filename: tha name of the file to remove (case sensitive)
+*/
+router.post("/files/:filename/remove", auth, (req, res) => {
+	const { id } = req.user;
+	const { filename } = req.params;
+
+	// find the user
+	User.findOne({ _id: id }, (err, user) => {
+		if (err || !user) {
+			return res.json({
+				success: false,
+				message: "Error in removing the file",
+			});
+		}
+		// check if the name of the file is in the user's list
+		if (!user.files.includes(filename)) {
+			return res.json({
+				success: false,
+				message: "File doesn't exist",
+			});
+		}
+
+		// remove the name of the file from the user's list
+		const fileIndex = user.files.indexOf(filename);
+		user.files.splice(fileIndex, 1);
+		user.save((err) => {
+			// delete the file from the folder
+			fs.unlink(uploadsPath + id + "/" + filename, (err) => {
+				res.json({ success: true, message: "File removed" });
+			});
 		});
 	});
 });
